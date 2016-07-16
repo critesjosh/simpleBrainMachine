3a0d8ce6f77652b6ef6b81b4474b6e4b07687684
Josh <critesjosh@gmail.com>
2016-07-15 18:11:10 -0600

    functional version!
    Added the time intervals and got the graphics and audio to render
    correctly.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 
 /* Brainwave Type
- a = alpha
- b = beta
- t = theta
- d = delta
+ a = alpha (7.5 - 12.5 Hz)
+ b = beta (12.5 - 30 Hz)
+ t = theta (4 – 7 Hz)
+ d = delta (0.1 – 3 Hz)
+ g = gamma 40 Hz
 
  // table of values for meditation
  //   start with lots of Beta (awake / conscious)
@@ -13,100 +14,212 @@
  //   pulse in some Delta (creativity)
  //   and then reverse the above to come up refreshed
 
- Duration of this Brainwave Type (divide by 10,000 to get seconds)
+ Duration of this Brainwave Type (seconds)
 
- { 'b', 600000 },
- { 'a', 100000 },
- { 'b', 200000 },
- { 'a', 150000 },
- { 'b', 150000 },
- { 'a', 200000 },
- { 'b', 100000 },
- { 'a', 300000 },
- { 'b',  50000 },
- { 'a', 600000 },
- { 't', 100000 },
- { 'a', 300000 },
- { 't', 200000 },
- { 'a', 300000 },
- { 't', 300000 },
- { 'a', 150000 },
- { 't', 600000 },
- { 'a', 150000 },
- { 'b',  10000 },
- { 'a', 150000 },
- { 't', 600000 },
- { 'd',  10000 },
- { 't', 100000 },
- { 'd',  10000 },
- { 't', 100000 },
- { 'd',  10000 },
- { 't', 300000 },
- { 'a', 150000 },
- { 'b',  10000 },
- { 'a', 150000 },
- { 't', 300000 },
- { 'a', 150000 },
- { 'b',  10000 },
- { 'a', 200000 },
- { 'b',  50000 },
- { 'a', 200000 },
- { 'b', 150000 },
- { 'a', 150000 },
- { 'b', 200000 },
- { 'a', 100000 },
- { 'b', 250000 },
- { 'a',  50000 },
- { 'b', 600000 },
+ { 'b', 60 },
+ { 'a', 10 },
+ { 'b', 20 },
+ { 'a', 15 },
+ { 'b', 15 },
+ { 'a', 20 },
+ { 'b', 10 },
+ { 'a', 30 },
+ { 'b',  5 },
+ { 'a', 60 },
+ { 't', 10 },
+ { 'a', 30 },
+ { 't', 20 },
+ { 'a', 30 },
+ { 't', 30 },
+ { 'a', 15 },
+ { 't', 60 },
+ { 'a', 15 },
+ { 'b',  1 },
+ { 'a', 15 },
+ { 't', 60 },
+ { 'd',  1 },
+ { 't', 10 },
+ { 'd',  1 },
+ { 't', 10 },
+ { 'd',  1 },
+ { 't', 30 },
+ { 'a', 15 },
+ { 'b',  1 },
+ { 'a', 15 },
+ { 't', 30 },
+ { 'a', 15 },
+ { 'b',  1 },
+ { 'a', 20 },
+ { 'b',  5 },
+ { 'a', 20 },
+ { 'b', 15 },
+ { 'a', 15 },
+ { 'b', 20 },
+ { 'a', 10 },
+ { 'b', 25 },
+ { 'a',  5 },
+ { 'b', 60 },
  { '0',      0 }
+
+ 856 seconds = 14.26666 minutes
  */
+var brainMachine = function(brainWave) {
+  var hz;
 
-//creat the audio context
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  switch (brainWave) {
+    case 'a':
+      hz = 10;
+      break;
+    case 'b':
+      hz = 25;
+      break;
+    case 't':
+      hz = 5;
+      break;
+    case 'd':
+      hz = 1.5;
+      break;
+  }
 
-//create and oscillator that will be the data for the sound
-var oscillatorLeft = audioCtx.createOscillator();
-var oscillatorRight = audioCtx.createOscillator();
-var gainNodeLeft = audioCtx.createGain();
-var gainNodeRight = audioCtx.createGain();
+  var visualFrequency = (1/hz) * 1000;
 
-//connect the oscillator to volume control
-oscillatorLeft.connect(gainNodeLeft);
-gainNodeLeft.connect(audioCtx.destination);
-oscillatorRight.connect(gainNodeRight);
-gainNodeRight.connect(audioCtx.destination);
+  //create and oscillator that will be the data for the sound
+  var oscillatorLeft = audioCtx.createOscillator();
+  var oscillatorRight = audioCtx.createOscillator();
+  var gainNodeLeft = audioCtx.createGain();
+  var gainNodeRight = audioCtx.createGain();
 
-//volume
-gainNodeLeft.gain.value = 0.1;
-gainNodeRight.gain.value = 0.1;
+  //connect the oscillator to volume control
+  oscillatorLeft.connect(gainNodeLeft);
+  gainNodeLeft.connect(audioCtx.destination);
+  oscillatorRight.connect(gainNodeLeft);
+  gainNodeRight.connect(audioCtx.destination);
 
-oscillatorLeft.type = 'sine';
-var leftFrequency = oscillatorLeft.frequency.value;
-leftFrequency = 505;
-oscillatorLeft.start();
+  //volume
+  gainNodeLeft.gain.value = 0.01;
+  gainNodeRight.gain.value = 0.01;
 
-oscillatorRight.type = 'sine';
-var rightFrequency = oscillatorRight.frequency.value;
-rightFrequency = 500;
-oscillatorRight.start();
+  //start leftFrequency
+  oscillatorLeft.type = 'sine';
+  oscillatorRight.type = 'sine';
+  var leftFrequency = oscillatorLeft.frequency.value;
+  var rightFrequency = oscillatorRight.frequency.value;
+  leftFrequency = 500;
+  rightFrequency = leftFrequency + hz;
+  oscillatorLeft.start();
+  oscillatorRight.start();
 
+  //animation to 'blink' light going to the left and right eyes.
+  var switchColors = function() {
+    $('#div').toggleClass('div');
+  }
 
-
-var myArrayBuffer = audioCtx.createBuffer(2, 22050, 44100);
-
-//var bufferData = myArrayBuffer.getChannelData(channel);
-
-var source = audioCtx.createBufferSource();
-source.buffer = myArrayBuffer;
-source.connect(audioCtx.destination);
-source.start();
-
-
-//animation to 'blink' light going to the left and right eyes.
-var switchColors = function() {
-  $('#div').toggleClass('div');
+  timeoutID = window.setInterval(function(){
+    switchColors();
+  }, visualFrequency);
 }
 
-window.setInterval(function(){
-switchColors();
-}, 50);
+var timeoutID;
+
+var clear = function(){
+  window.clearTimeout(timeoutID);
+}
+
+var interval = function(brainWave, time) {
+    console.log(brainWave);
+    clear();
+    brainMachine(brainWave);
+}
+
+//create the audio context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+var seconds = 60;
+
+interval('b');
+var time = 1000 * seconds;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 10000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 20000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 20000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 10000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 30000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 5000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 60000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 10000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 30000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 20000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 30000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 30000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 60000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 1000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 60000;
+window.setTimeout(function(){ return interval('d'); }, time);
+time += 1000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 10000;
+window.setTimeout(function(){ return interval('d'); }, time);
+time += 1000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 10000;
+window.setTimeout(function(){ return interval('d'); }, time);
+time += 1000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 30000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 1000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('t'); }, time);
+time += 30000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 1000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 20000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 5000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 20000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 15000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 20000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 10000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 25000;
+window.setTimeout(function(){ return interval('a'); }, time);
+time += 5000;
+window.setTimeout(function(){ return interval('b'); }, time);
+time += 60000;
+window.setTimeout(function(){ return interval('b'); }, time);
